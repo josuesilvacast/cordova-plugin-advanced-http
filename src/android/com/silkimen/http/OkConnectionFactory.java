671e89a794d80dff118809be3f0bfe52b8dc9d25
josue.silva <josue.silva@castgroup.com.br>
2022-10-25 17:21:41 -0300

    adjustement OKConnectionFactory

--- src/android/com/silkimen/http/OkConnectionFactory.java
+++ src/android/com/silkimen/http/OkConnectionFactory.java
@@ -1,26 +1,25 @@
 package com.silkimen.http;
 
 import okhttp3.OkHttpClient;
-import okhttp3.OkUrlFactory;
 
+import java.io.IOException;
 import java.net.URL;
 import java.net.HttpURLConnection;
 import java.net.URLStreamHandler;
 import java.net.Proxy;
 
 public class OkConnectionFactory implements HttpRequest.ConnectionFactory {
-  private final OkHttpClient client = new OkHttpClient();
 
-  public HttpURLConnection create(URL url) {
-    OkUrlFactory urlFactory = new OkUrlFactory(this.client);
+  public HttpURLConnection create(URL url) throws IOException {
 
-    return (HttpURLConnection) urlFactory.open(url);
+    return (HttpURLConnection) url.openConnection();
   }
 
-  public HttpURLConnection create(URL url, Proxy proxy) {
+  public HttpURLConnection create(URL url, Proxy proxy) throws IOException {
     OkHttpClient clientWithProxy = new OkHttpClient.Builder().proxy(proxy).build();
-    OkUrlFactory urlFactory = new OkUrlFactory(clientWithProxy);
+    HttpURLConnection urlFactory = (HttpURLConnection) url.openConnection(proxy);
 
-    return (HttpURLConnection) urlFactory.open(url);
+    return urlFactory;
   }
 }
+
